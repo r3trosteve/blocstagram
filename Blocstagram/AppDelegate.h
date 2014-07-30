d50bf0b4e78fb8ab1822f93ae105a5cb4ca0791e
Steve Schofield <stevebschofield@gmail.com>
2014-07-30 06:56:48 +0100

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Steven Schofield on 30/07/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Steven Schofield on 30/07/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
