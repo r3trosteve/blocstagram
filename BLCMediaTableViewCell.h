f3135c2823fe4663ffc3bed97bc12322cf141a2e
Steve Schofield <stevebschofield@gmail.com>
2014-07-31 22:22:22 +0100

    Switched to a custom table view cell

---
+++ BLCMediaTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  BLCMediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Steven Schofield on 31/07/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BLCMedia;
+
+@interface BLCMediaTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) BLCMedia *mediaItem;
+
++ (CGFloat) heightForMediaItem:(BLCMedia *)mediaItem width:(CGFloat)width;
+
+@end
--- Blocstagram/BLCImagesTableViewController.m
+++ Blocstagram/BLCImagesTableViewController.m
@@ -11,6 +11,7 @@
 #import "BLCMedia.h"
 #import "BLCUser.h"
 #import "BLCComment.h"
+#import "BLCMediaTableViewCell.h"
 
 @interface BLCImagesTableViewController ()
 
@@ -34,8 +35,9 @@
 {
     [super viewDidLoad];
     
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"imageCell"];
+    [self.tableView registerClass:[BLCMediaTableViewCell class] forCellReuseIdentifier:@"mediaCell"];
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -57,25 +59,8 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
-    
-    static NSInteger imageViewTag = 1234;
-    UIImageView *imageView = (UIImageView*)[cell.contentView viewWithTag:imageViewTag];
-    
-    if (!imageView) {
-        // This is a new cell, it doesn't have an image view yet
-        imageView = [[UIImageView alloc] init];
-        imageView.contentMode = UIViewContentModeScaleToFill;
-        
-        imageView.frame = cell.contentView.bounds;
-        imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-        
-        imageView.tag = imageViewTag;
-        [cell.contentView addSubview:imageView];
-    }
-    
-    BLCMedia *item = self.items[indexPath.row];
-    imageView.image = item.image;
+    BLCMediaTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"mediaCell" forIndexPath:indexPath];
+    cell.mediaItem = [BLCDataSource sharedInstance].mediaItems[indexPath.row];
     
     return cell;
 }
@@ -83,11 +68,8 @@
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     BLCMedia *item = self.items[indexPath.row];
-    UIImage *image = item.image;
-    
-    return (CGRectGetWidth(self.view.frame) / image.size.width) * image.size.height;
+    return [BLCMediaTableViewCell heightForMediaItem:item width:CGRectGetWidth(self.view.frame)];
 }
-
 
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
@@ -109,6 +91,7 @@
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
+
 
 
 /*
